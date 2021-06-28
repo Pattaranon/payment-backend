819ce62e56eb4f70709465010d3f12c35e8ccb0a
Pattaranon Plyduang <pattaranon.ply@gmail.com>
2021-06-28 12:18:03 +0700

    First commit for checklist payment backend

---
+++ src/test/java/com/space/payment/PaymentBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.space.payment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentBackendApplicationTests {
+
+	@Test
+	public void testInitialMethod() {
+		Assertions.assertEquals("1", "1");
+	}
+
+}
